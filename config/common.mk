ad4dc607dec473816b3e9bc870f7ed63cd68f93a
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-03-26 08:15:14 +0100

    Allow A/B OTA downgrades on debuggable builds
    Change-Id: I361c6d71f7b4316cf023ad0b36fdc488e37d6176

--- config/common.mk
+++ config/common.mk
@@ -36,6 +36,12 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/aosp/prebuilt/common/bin/50-base.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-base.sh
 
+# OTA
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.ota.allow_downgrade=true
+endif
+
 ifneq ($(AB_OTA_PARTITIONS),)
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
