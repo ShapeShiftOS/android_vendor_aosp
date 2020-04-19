913842cb0087bd8f0508601e8e03cd0366df91dc
mikeNG <mkbestas@lineageos.org>
2020-04-19 00:50:03 -0500

    vendor: config: Replace 'system' with '$(TARGET_COPY_OUT_SYSTEM)'
    Also, move product to $(TARGET_COPY_OUT_PRODUCT)
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Change-Id: I0a71b3728f36501993b74c13d2c98de7061244af
    
    .

--- config/ota.mk
+++ config/ota.mk
@@ -9,6 +9,5 @@ CUSTOM_PROPERTIES += \
 #    Updates
 
 PRODUCT_COPY_FILES += \
-    vendor/aosp/config/permissions/org.extendedui.ota.xml:system/etc/permissions/org.extendedui.ota.xml
-
+    vendor/aosp/config/permissions/org.extendedui.ota.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.extendedui.ota.xml
 endif
