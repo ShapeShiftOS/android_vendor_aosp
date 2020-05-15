CesiumOS-org/vendor_cesium

vendor: priv-app: Whitelist Google permissions
E AndroidRuntime: java.lang.IllegalStateException: Signature|privileged permissions not in privapp-permissions whitelist: {com.android.vending: android.permission.CHANGE_OVERLAY_PACKAGES, com.google.android.apps.turbo: android.permission.POWER_SAVER, com.google.android.apps.pixelmigrate: android.permission.SUBSTITUTE_NOTIFICATION_APP_NAME, com.google.android.setupwizard: android.permission.START_ACTIVITIES_FROM_BACKGROUND, com.android.vending: android.permission.SEND_DEVICE_CUSTOMIZATION_READY, com.android.vending: android.permission.START_ACTIVITIES_FROM_BACKGROUND, com.google.android.apps.pixelmigrate: android.permission.READ_PRIVILEGED_PHONE_STATE, com.google.android.apps.pixelmigrate: android.permission.WRITE_SECURE_SETTINGS, com.google.android.apps.pixelmigrate: android.permission.BACKUP, com.android.vending: android.permission.READ_RUNTIME_PROFILES, com.google.android.googlequicksearchbox: android.permission.MANAGE_USB, com.google.android.setupwizard: android.permission.SUBSTITUTE_NOTIFICATION_APP_NAME, com.google.android.apps.pixelmigrate: android.permission.WRITE_EMBEDDED_SUBSCRIPTIONS, com.google.android.googlequicksearchbox: android.permission.START_ACTIVITIES_FROM_BACKGROUND, com.google.android.googlequicksearchbox: android.permission.PACKAGE_USAGE_STATS}

Change-Id: I82d801ba109ee680b760bfc3f8f02d01c51ffe57
Signed-off-by: Jackeagle <jackeagle102@gmail.com>
Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

config/common.mk
@@ -70,7 +70,8 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/config/permissions/privapp-permissions-fm.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-fm.xml \
     vendor/aosp/config/permissions/privapp-permissions-snap.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-snap.xml \
     vendor/aosp/config/permissions/privapp-permissions-camera2.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-camera2.xml \
-    vendor/aosp/config/permissions/privapp-permissions-cesium-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-cesium-product.xml
+    vendor/aosp/config/permissions/privapp-permissions-cesium-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-cesium-product.xml \
+    vendor/aosp/config/permissions/privapp-permissions-google_prebuilt.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google_prebuilt.xml
 
 # Copy all custom init rc files
 $(foreach f,$(wildcard vendor/aosp/prebuilt/common/etc/init/*.rc),\

ShapeShiftOS/android_vendor_aosp

config/common.mk
@@ -53,7 +53,8 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/config/permissions/privapp-permissions-fm.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-fm.xml \
     vendor/aosp/config/permissions/privapp-permissions-snap.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-snap.xml \
     vendor/aosp/config/permissions/privapp-permissions-camera2.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-camera2.xml \
-    vendor/aosp/config/permissions/privapp-permissions-exui-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-exui-product.xml
+    vendor/aosp/config/permissions/privapp-permissions-exui-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-exui-product.xml \
+    vendor/aosp/config/permissions/privapp-permissions-google_prebuilt.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google_prebuilt.xml
 
 # Copy all custom init rc files
 $(foreach f,$(wildcard vendor/aosp/prebuilt/common/etc/init/*.rc),\