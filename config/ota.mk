8956dd8776b91507bb21b6c9bbef7dc8c2e8a83f
AshutoshSundresh <62063875+AshutoshSundresh@users.noreply.github.com>
2020-06-27 18:43:33 +0530

    Remove OTA Updater again

--- config/ota.mk
+++ config/ota.mk
@@ -5,7 +5,7 @@ CUSTOM_OTA_VERSION_CODE := android_10
 CUSTOM_PROPERTIES += \
     org.extendedui.ota.version_code=$(CUSTOM_OTA_VERSION_CODE)
 
-PRODUCT_PACKAGES += \
-    Updates
+#PRODUCT_PACKAGES += \
+#    Updates
 
 endif
