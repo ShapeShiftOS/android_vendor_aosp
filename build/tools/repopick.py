LineageOS/android_vendor_lineage

repopick: Address PEP8 warnings regarding indentations and variable naming
- Rename variables to lowercase
- Add spacing between # and text

Ref:
[0]: https://www.python.org/dev/peps/pep-0008/#function-and-variable-names
[1]: https://www.python.org/dev/peps/pep-0008/#block-comments

Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
Change-Id: I16dedf465674056571782921983eb3ec4276a1f8

build/tools/repopick.py
@@ -89,7 +89,7 @@ def fetch_query_via_ssh(remote_url, query):
                         }
                     },
                     'commit': {
-                        'parents': [{ 'commit': parent } for parent in patch_set['parents']]
+                        'parents': [{'commit': parent} for parent in patch_set['parents']]
                     },
                 } for patch_set in data['patchSets']},
                 'subject': data['subject'],
@@ -112,13 +112,13 @@ def fetch_query_via_http(remote_url, query):
                 parts = line.rstrip().split("|")
                 if parts[0] in remote_url:
                     auth = requests.auth.HTTPBasicAuth(username=parts[1], password=parts[2])
-        statusCode = '-1'
+        statuscode = '-1'
         if auth:
             url = f'{remote_url}/a/changes/?q={query}&o=CURRENT_REVISION&o=ALL_REVISIONS&o=ALL_COMMITS'
             data = requests.get(url, auth=auth)
-            statusCode = str(data.status_code)
-        if statusCode != '200':
-            #They didn't get good authorization or data, Let's try the old way
+            statuscode = str(data.status_code)
+        if statuscode != '200':
+            # They didn't get good authorization or data, Let's try the old way
             url = f'{remote_url}/changes/?q={query}&o=CURRENT_REVISION&o=ALL_REVISIONS&o=ALL_COMMITS'
             data = requests.get(url)
         reviews = json.loads(data.text[5:])
@@ -142,6 +142,7 @@ def fetch_query(remote_url, query):
         return fetch_query_via_http(remote_url, query.replace(' ', '+'))
     else:
         raise Exception('Gerrit URL should be in the form http[s]://hostname/ or ssh://[user@]host[:port]')
+
 
 if __name__ == '__main__':
     # Default to LineageOS Gerrit
@@ -246,8 +247,8 @@ if __name__ == '__main__':
     remotes = xml_root.findall('remote')
     default_revision = xml_root.findall('default')[0].get('revision')
 
-    #dump project data into the a list of dicts with the following data:
-    #{project: {path, revision}}
+    # dump project data into the a list of dicts with the following data:
+    # {project: {path, revision}}
 
     for project in projects:
         name = project.get('name')

ShapeShiftOS/android_vendor_aosp

- Format variables into snake case
- Space after ','
- Test for membership with "not in" instead
- Remove too many empty lines
[2]: http://pep8online.com/
(cherry picked from commit 1f1a4c82b3e7b183f9a018d359750890eca36cdb)

build/tools/repopick.py
@@ -76,7 +76,6 @@ def fetch_query_via_ssh(remote_url, query):
     else:
         raise Exception('Malformed URI: Expecting ssh://[user@]host[:port]')
 
-
     out = subprocess.check_output(['ssh', '-x', '-p{0}'.format(port), userhost, 'gerrit', 'query', '--format=JSON --patch-sets --current-patch-set', query])
     if not hasattr(out, 'encode'):
         out = out.decode()
@@ -99,7 +98,7 @@ def fetch_query_via_ssh(remote_url, query):
                         }
                     },
                     'commit': {
-                        'parents': [{ 'commit': parent } for parent in patch_set['parents']]
+                        'parents': [{'commit': parent} for parent in patch_set['parents']]
                     },
                 } for patch_set in data['patchSets']},
                 'subject': data['subject'],
@@ -122,12 +121,12 @@ def fetch_query_via_http(remote_url, query):
                 parts = line.rstrip().split("|")
                 if parts[0] in remote_url:
                     auth = requests.auth.HTTPBasicAuth(username=parts[1], password=parts[2])
-        statusCode = '-1'
+        status_code = '-1'
         if auth:
             url = '{0}/a/changes/?q={1}&o=CURRENT_REVISION&o=ALL_REVISIONS&o=ALL_COMMITS'.format(remote_url, query)
             data = requests.get(url, auth=auth)
-            statusCode = str(data.status_code)
-        if statusCode != '200':
+            status_code = str(data.status_code)
+        if status_code != '200':
             #They didn't get good authorization or data, Let's try the old way
             url = '{0}/changes/?q={1}&o=CURRENT_REVISION&o=ALL_REVISIONS&o=ALL_COMMITS'.format(remote_url, query)
             data = requests.get(url)
@@ -152,6 +151,7 @@ def fetch_query(remote_url, query):
         return fetch_query_via_http(remote_url, query.replace(' ', '+'))
     else:
         raise Exception('Gerrit URL should be in the form http[s]://hostname/ or ssh://[user@]host[:port]')
+
 
 if __name__ == '__main__':
     # Default to PixelExperience Gerrit
@@ -256,8 +256,8 @@ if __name__ == '__main__':
     remotes = xml_root.findall('remote')
     default_revision = xml_root.findall('default')[0].get('revision')
 
-    #dump project data into the a list of dicts with the following data:
-    #{project: {path, revision}}
+    # dump project data into the a list of dicts with the following data:
+    # {project: {path, revision}}
 
     for project in projects:
         name = project.get('name')
@@ -271,7 +271,7 @@ if __name__ == '__main__':
             if revision is None:
                 revision = default_revision
 
-        if not name in project_name_to_data:
+        if name not in project_name_to_data:
             project_name_to_data[name] = {}
         revision = revision.split('refs/heads/')[-1]
         if path is None:
@@ -414,7 +414,7 @@ if __name__ == '__main__':
             output = output.split()
             if 'Change-Id:' in output:
                 head_change_id = ''
-                for j,t in enumerate(reversed(output)):
+                for j, t in enumerate(reversed(output)):
                     if t == 'Change-Id:':
                         head_change_id = output[len(output) - j]
                         break