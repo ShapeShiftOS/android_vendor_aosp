AICP/vendor_aicp

vendor/aicp: Toggle-able adblock hosts file [2/3]
Regenerate with ./adaway/generate.sh

Change-Id: Ia389457746f9af1060a1c1ae62af0cb9ca643a71

config/common.mk
@@ -137,5 +137,9 @@ PRODUCT_COPY_FILES += \
     vendor/aicp/prebuilt/common/lib/libsketchology_native.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsketchology_native.so \
     vendor/aicp/prebuilt/common/lib64/libsketchology_native.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsketchology_native.so
 
+# Ad-block hosts
+PRODUCT_COPY_FILES += \
+    vendor/aicp/prebuilt/common/etc/hosts.aicp_adblock:system/etc/hosts.aicp_adblock
+
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false

ShapeShiftOS/android_vendor_aosp

config/common.mk
@@ -193,3 +193,7 @@ include vendor/google-customization/config.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
+# Ad-block hosts
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/hosts.aicp_adblock:system/etc/hosts.aicp_adblock
+