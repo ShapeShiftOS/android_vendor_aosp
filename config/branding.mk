95ddbe5b0561049b7b48135a043c30fba007a118
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-10-08 19:00:43 +0530

    Final Android Q release (2/4)

--- config/branding.mk
+++ config/branding.mk
@@ -1,7 +1,7 @@
 # Set all versions
 CUSTOM_BUILD_TYPE ?= UNOFFICIAL
 
-EXUI_BASE_VERSION = Blaziken-1.2
+EXUI_BASE_VERSION = Blaziken-1.2.1
 
 CUSTOM_DATE_YEAR := $(shell date -u +%Y)
 CUSTOM_DATE_MONTH := $(shell date -u +%m)
